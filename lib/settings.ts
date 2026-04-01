b1652f174d23d2196cbd16e2a14fac0e42fd25b2
Choco Li <trust2065@gmail.com>
2026-04-01 13:19:23 +1100

    Default translate to zh-Hant

--- lib/settings.ts
+++ lib/settings.ts
@@ -1,46 +1,46 @@
-import { OptionalKeysOf } from 'type-fest'
-import { DefaultLLMPrompt } from './translate/openai'
-import { type ToLang } from './translate/lang'
+import { OptionalKeysOf } from 'type-fest';
+import { DefaultLLMPrompt } from './translate/openai';
+import { type ToLang } from './translate/lang';
 
 export interface Settings {
-  to?: ToLang
-  engine?: 'microsoft' | 'openai'
+  to?: ToLang;
+  engine?: 'microsoft' | 'openai';
 
-  'openai.apiKey'?: string
-  'openai.model'?: string
-  'openai.baseUrl'?: string
-  'openai.prompt'?: string
+  'openai.apiKey'?: string;
+  'openai.model'?: string;
+  'openai.baseUrl'?: string;
+  'openai.prompt'?: string;
 }
 
 export function getDefaultSettings(): Pick<Settings, OptionalKeysOf<Settings>> {
   return {
-    to: 'en',
+    to: 'zh-Hant',
     engine: 'microsoft',
     'openai.baseUrl': 'https://api.openai.com/v1',
     'openai.prompt': DefaultLLMPrompt,
     'openai.model': 'gpt-4.1-mini',
-  }
+  };
 }
 
 export async function getMergedSettings(): Promise<Settings> {
   return Object.freeze({
     ...getDefaultSettings(),
     ...(await getSyncSettings()),
-  })
+  });
 }
 
 export async function getSyncSettings(): Promise<Settings> {
   return {
     ...(
       await browser.storage.sync.get<{
-        settings: Settings
+        settings: Settings;
       }>(['settings'])
     ).settings,
-  }
+  };
 }
 
 export async function setSyncSettings(settings: Settings) {
   await browser.storage.sync.set<{
-    settings: Settings
-  }>({ settings })
+    settings: Settings;
+  }>({ settings });
 }
